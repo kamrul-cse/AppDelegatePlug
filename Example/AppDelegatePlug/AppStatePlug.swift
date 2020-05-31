ad1737c4d43743a88a96ff75fb6d2dffefff4659
MKHG Lab <mkhglab@gmail.com>
2020-05-31 21:39:26 +0600

    first commit

---
+++ Example/AppDelegatePlug/AppStatePlug.swift
@@ -0,0 +1,9 @@
+//
+//  AppStatePlug.swift
+//  AppDelegatePlug_Example
+//
+//  Created by Md. Kamrul Hasan on 31/5/20.
+//  Copyright © 2020 CocoaPods. All rights reserved.
+//
+
+import Foundation
